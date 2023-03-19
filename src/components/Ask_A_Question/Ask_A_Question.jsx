662b4fd3bef19475310f30ff2d6dc090d42562e6
BlazerXXI <ruslandendik@gmail.com>
2023-03-19 21:55:18 +0200

    dev ask

--- src/components/Ask_A_Question/Ask_A_Question.jsx
+++ src/components/Ask_A_Question/Ask_A_Question.jsx
@@ -8,13 +8,21 @@ export const Ask_A_Question = () => {
 				<div className="question-block__question-bait">
 					<h2 className="question-bait__overhead bait-overhead">Кто будет готовить?</h2>
 					<h3 className="question-bait__title bait-title">Лично приготовлю и всё красиво упакую для вашего события</h3>
-					<p className="question-bait__sup-title bait-sup">Загрузите фотографию или эскиз капкейков и мы рассчитаем стоимость за 30 минут</p>
+					<ul>
+						<li className="question-bait__sup-title bait-sup">Проконсультирую по выбору капкейкови придумаю нестандартную идею</li>
+						<li className="question-bait__sup-title bait-sup">Приготовлю капкейки для вашего события, которые обязательно всем понравятся </li>
+						<li className="question-bait__sup-title bait-sup">Аккуратно и красиво всё упакую, если вы хотите сделать приятный подарок</li>
+					</ul>
 				</div>
 				<div className="question-block__link-row question-link-row bait-link-row">
 					<button className="question-link-row__link bait-link">Загрузить фотографию</button>
 				</div>
 			</div>
 			<div className="question-woman">
+				<div className="question-woman__name-block">
+					<h2>Юлия Кондратьева</h2>
+					<p>ваш кондитер</p>
+				</div>
 				<img src={woman} alt="" />
 			</div>
 		</div>
